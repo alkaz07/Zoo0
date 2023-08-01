8c23ca279cb718e4ab3d985432ed7c77a6c43e22
Ирина <tiptop100@mail.ru>
2023-08-01 16:42:59 +0300

    добавлена lisa

--- src/Fox.java
+++ src/Fox.java
@@ -2,7 +2,7 @@ public class Fox extends Animal  {
     public void say()
     {
         double weight=12;
-        System.out.println("Бегает");
+        System.out.println("Уф Уф");
         System.out.println("Вес" + weight);
 
     }
